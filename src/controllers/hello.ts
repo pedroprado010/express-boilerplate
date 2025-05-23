65be0be051b1d9d28ae74b9ff5a9e173422df784
Pedro Prado <pedro.salomon.contractor@macmillan.com>
2025-05-23 14:37:47 -0300

    check db

--- src/controllers/hello.ts
+++ src/controllers/hello.ts
@@ -1,6 +1,11 @@
 import { RequestHandler } from 'express';
+import { db } from '../utils/database.js';
 
-export const hello: RequestHandler = (req, res) => {
+export const hello: RequestHandler = async (req, res) => {
   res.status(200);
+  
+  const dbTest = await db.raw('select \'1\'');
+
+  console.log({ dbTest });
   res.json({ good: 1 });
 };
